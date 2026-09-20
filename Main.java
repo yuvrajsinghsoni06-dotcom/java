b09835c01c6a963af1cab5c63b7c5a755ac4c064
yuvrajsonigreat <yuvrajsinghsoni06@gmail.com>
2026-09-20 14:32:16 +0530

    Refactor Main class to implement Mad Libs game and remove area calculation code

--- Main.java
+++ Main.java
@@ -22,17 +22,47 @@ public class Main {
         // }
         // scanner.close();
 
-        double Length = 0;
-        double width = 0;
+        // double Length = 0;
+        // double width = 0;
 
-        System.out.println("Enter Length:" );
-        Length = scanner.nextDouble();
-        System.out.println("Enter width:" );
-        width = scanner.nextDouble();
+        // System.out.println("Enter Length:" );
+        // Length = scanner.nextDouble();
+        // System.out.println("Enter width:" );
+        // width = scanner.nextDouble();
 
-        double area = Length * width;
-        System.out.println("The area of the rectangle is: " + area);
+        // double area = Length * width;
+        // System.out.println("The area of the rectangle is: " + area);
 
+        // scanner.close();
+
+        // MAd LAb Game
+
+        String adjective1;
+        String adjective2;
+        String adjective3;
+        String noun1;
+        String verb1;
+
+
+        System.out.println("Enter an adjective (description): ");
+        adjective1 = scanner.nextLine();
+        System.out.println("Enter a noun (person or animal): ");
+        noun1 = scanner.nextLine();
+        System.out.println("Enter an adjective (description): ");
+        adjective2 = scanner.nextLine();
+        System.out.println("Enter a verb ends with ing (action): ");
+        verb1 = scanner.nextLine();
+        System.out.println("Enter an adjective (description): ");
+        adjective3 = scanner.nextLine();
+
+
+        System.out.println("Today I went To a " + adjective1 + " " + "Zoo");
+        System.out.println("In an exhibition ,i saw a " + noun1 + ".");
+        System.out.println(noun1 + "was" + adjective2 + "and" +  verb1 + "!");
+        System.out.println("I was  " + adjective3 + "!");
+
+ 
+        scanner.close();
 
 
 
