6325b3ace4ddac7d2e1631963cc6fcc80715a936
yuvrajsonigreat <yuvrajsinghsoni06@gmail.com>
2026-09-24 17:59:04 +0530

    Refactor Main class to enable user input for triangle sides and calculate area, while commenting out random boolean functionality

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-import java.util.Random;
+// import java.util.Random;
 
 public class Main {
     public static void main(String[] args){
@@ -165,26 +165,81 @@ public class Main {
 
         // scanner.close();
 
-        Random random  = new Random();
+        // Random random  = new Random();
 
-        Boolean isHead;
-        // double number2;
-        // double number3;
+        // Boolean isHead;
+        // // double number2;
+        // // double number3;
 
-        isHead = random.nextBoolean();
+        // isHead = random.nextBoolean();
 
-        // number2 = random.nextDouble(1,100);
+        // // number2 = random.nextDouble(1,100);
 
-        // number3 = random.nextDouble(1,100);
-        System.out.println(isHead);
-        // System.out.println(number2);
-        // System.out.println(number3);
+        // // number3 = random.nextDouble(1,100);
+        // System.out.println(isHead);
+        // // System.out.println(number2);
+        // // System.out.println(number3);
 
-        if(isHead){
-            System.out.println("Heads");
-        }else{
-            System.out.println("Tails");
-        }
+        // if(isHead){
+        //     System.out.println("Heads");
+        // }else{
+        //     System.out.println("Tails");
+        // }
+
+        // maths constants
+
+        // System.out.println(Math.PI);
+        // System.out.println(Math.E);
+
+        // double result;
+
+        // result = Math.pow(2,5);
+        // result = Math.abs(-5);
+        // result = Math.sqrt(9);
+        // result = Math.round(3.14);
+        // result = Math.ceil(3.14);
+        // result = Math.floor(3.99);
+        // result = Math.max(10,20);
+        // result = Math.min(10,20);
+
+
+        // System.out.println(result);
+
+        Scanner scanner = new Scanner(System.in);
+
+        //excercise -1
+        double a;
+        double b;
+        double c;
+        
+
+        System.out.println("Enter the value for A: ");
+        a = scanner.nextDouble();
+
+        System.out.println("Enter the value for B : ");
+        b = scanner.nextDouble();
+
+        a = Math.pow(a,2);
+        b = Math.pow(b,2);
+
+        c = Math.sqrt(a + b);
+
+        
+        
+        ;
+        
+
+        System.out.println("Area of Right Angle triangle is: " + " " +  c);
+
+        scanner.close();
+
+
+
+
+
+
+
+
 
 
         
